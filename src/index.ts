f60ce58aa5a53845550e7a868f55cdfde7bc7adf
Giulio Fagioli <fagioli.giulio@gmail.com>
2026-04-04 08:48:44 +0200

    feat: add rename command and register it
    ccm rename <old-name> <new-name> with validation, error handling, success output.

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { registerCreate } from './commands/create.js'
 import { registerList } from './commands/list.js'
 import { registerLogin } from './commands/login.js'
 import { registerRemove } from './commands/remove.js'
+import { registerRename } from './commands/rename.js'
 import { registerRun } from './commands/run.js'
 import { registerStatus } from './commands/status.js'
 import { registerUse } from './commands/use.js'
@@ -17,6 +18,7 @@ const program = new Command()
 registerCreate(program)
 registerList(program)
 registerRemove(program)
+registerRename(program)
 registerLogin(program)
 registerStatus(program)
 registerUse(program)
